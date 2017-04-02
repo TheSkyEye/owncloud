6108fb7d35a4089827f1996f46d20a5dcf7a2fe4
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2017-04-02 12:14:56 +0200

    Update install _owncloud.sh

--- install _owncloud.sh
+++ install _owncloud.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get upgrade -y
-#apt-get install -y nginx php5-fpm php5-curl php5-mysql php5-gd php5-xmlrpc php5-gd php5-json php5-intl php5-mcrypt php5-imagick php5-ldap mariadb-server mariadb-client php-xml-parser
+apt-get install -y nginx php5 php5-fpm php5-curl php5-mysql php5-gd php5-xmlrpc php5-gd php5-json php5-intl php5-mcrypt php5-imagick php5-ldap mariadb-server mariadb-client php-xml-parser
 apt-get install -y nginx mariadb-server mariadb-client
 echo "deb http://dl.hhvm.com/debian jessie main" > /etc/apt/sources.list.d/hhvm.list
 wget -O- http://dl.hhvm.com/conf/hhvm.gpg.key | apt-key add -
